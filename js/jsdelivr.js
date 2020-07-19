9fac2759c975f1f428916e3afa2a04b780a44d48
timmyliang(梁伟添) <timmyliang@tencent.com>
2020-07-19 23:17:53 +0800

    Site updated: 2020-07-19 23:15:12

--- js/jsdelivr.js
+++ js/jsdelivr.js
@@ -1,23 +1,33 @@
-// 将图片路径改为 jsdelivr 的 cdn 路径
-
-var name = "FXTD-Odyssey";
-var repository = "FXTD-odyssey.github.io";
-if (document.domain != 'localhost') {
-    if (document.getElementById("post")) {
-        for (var key in document.getElementById("article-container").getElementsByTagName('img')) {
-            if (isNaN(key)) {
-                break;
-            }
-            var src = document.getElementById("article-container").getElementsByTagName('img')[key].src
-            if (src.startsWith("/post_img")) {
-                src = "cdn.jsdelivr.net/gh/" + name + "/" + repository + src
-            } else {
-                src = src.replace(document.domain, "cdn.jsdelivr.net/gh/" + name + "/" + repository)
-            }
-            document.getElementById("article-container").getElementsByTagName('img')[key].src = src
-        }
-    }
-}
+// NOTE 将图片路径改为 jsdelivr 的 cdn 路径
+$(function () {
+    var name = "FXTD-Odyssey";
+    var repository = "FXTD-odyssey.github.io";
+    var link;
+    var src;
+    $("img").each(function () {
+        src = $(this).attr("src")
+        if (src && (src.startsWith("/post_img") || src.startsWith("/img"))) {
+            link = "//cdn.jsdelivr.net/gh/" + name + "/" + repository + src
+            $(this).attr("src",link)
+        } 
+    })
+    // if (document.domain != 'localhost') {
+    //     if (document.getElementById("post")) {
+    //         for (var key in document.getElementById("article-container").getElementsByTagName('img')) {
+    //             if (isNaN(key)) {
+    //                 continue;
+    //             }
+    //             var src = document.getElementById("article-container").getElementsByTagName('img')[key].src
+    //             if (src.startsWith("/post_img")) {
+    //                 src = "cdn.jsdelivr.net/gh/" + name + "/" + repository + src
+    //             } else {
+    //                 src = src.replace(document.domain, "cdn.jsdelivr.net/gh/" + name + "/" + repository)
+    //             }
+    //             document.getElementById("article-container").getElementsByTagName('img')[key].src = src
+    //         }
+    //     }
+    // }
+})
 
 
 // Note 添加 comment 特殊样式
@@ -47,7 +57,15 @@ $(".comment").each(function () {
     }
 })
 // NOTE 单个链接自动居中
-$(".post-content>p").find("a").each(function() {
-    $(this).parent().css('text-align','center')
+$(".post-content>p").find("a").each(function () {
+    $(this).parent().css('text-align', 'center')
 })
 
+
+// NOTE 将封面视频转换为 video 标签
+$(".post_bg").each(function(){
+    var src = $(this).attr('data-src')
+    if (src.toString().endsWith(".mp4")){
+        $(this).replaceWith($('<video class="post_bg" src="' + src + '" autoplay="autoplay" loop="loop" style="width: 100%; height:100%;"></video>'));
+    }
+})
